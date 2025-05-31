51f132c7ada92634df3453f14c432ec63587eba8
ketroc <jwelsh1@gmail.com>
2025-05-31 15:37:20 -0400

    fix: possible fix to calcExpansions query

--- ocraft-s2client-bot/src/main/java/com/github/ocraft/s2client/bot/gateway/QueryInterface.java
+++ ocraft-s2client-bot/src/main/java/com/github/ocraft/s2client/bot/gateway/QueryInterface.java
@@ -326,7 +326,7 @@ public interface QueryInterface {
             float yMinDistCorner = isMineralNode ? 5f : 6;
             float xDist = Math.abs(nodePos.getX() - basePos.getX());
             float yDist = Math.abs(nodePos.getY() - basePos.getY());
-            if (xDist < yDist) {
+            if (xDist > yDist) {
                 if (xDist < xMinDistCorner && yDist < yMinDistCenter) {
                     return moveYFromNodeBy(basePos, nodePos, yMinDistCenter);
                 }
